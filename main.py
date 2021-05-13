8f91d25baacb5906a254498a436703b6853b28cf
ViniciusLeonadovky <vile.pb@gmail.com>
2021-05-12 22:22:52 -0300

    tratamento de dados qualitativos

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 from pandas_profiling import ProfileReport
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 df_steam = pd.read_csv('C:\\Users\\sioux\\Documents\\projects\\facul\\estudo_para_ac\\ac\\steam.csv')
 
@@ -9,7 +11,62 @@ df_steam = pd.read_csv('C:\\Users\\sioux\\Documents\\projects\\facul\\estudo_par
 #relatorio.to_file('C:\\Users\\sioux\\Documents\\projects\\facul\\estudo_para_ac\\ac\\steam_rel.html')
 
 newCategory = 'Single-Player'
+newGenre = 'Action'
+newTags = 'FPS'
 
-#categoryExists = lambda x: 18 if(np.isnan(x) or x < 18) else x
+# Feature alvo
+df_steam['alvo'] = df_steam['name'] 
+
+# Tratamento de dados qualitativos
+
 categoryExists = lambda x: 1 if(newCategory.lower() in x.lower()) else 0
 df_steam['pre_categories'] = df_steam['categories'].apply(categoryExists)
+
+genreExists = lambda x: 1 if(newGenre.lower() in x.lower()) else 0
+df_steam['pre_genre'] = df_steam['genres'].apply(genreExists)
+
+tagsExists = lambda x: 1 if(newTags.lower() in x.lower()) else 0
+df_steam['pre_steamspy_tags'] = df_steam['steamspy_tags'].apply(tagsExists)
+
+
+df_final = df_steam[[
+'alvo',
+'pre_categories',
+'pre_genre',
+'pre_steamspy_tags',
+]]
+
+# Demonstrar correlacoes
+
+df_steam.corr()
+
+# Separar dados Treino e Teste
+
+y = df_final['alvo']
+X = df_final[['pre_categories',
+'pre_genre',
+'pre_steamspy_tags']].values
+
+# knn
+
+nbrs = NearestNeighbors(n_neighbors=7, algorithm='ball_tree').fit(X)
+distancias, indices = nbrs.kneighbors(X)
+
+game = 8000
+
+print(X[game])
+print(distancias[game])
+print(indices[game])
+print('media distancia: ', np.mean(distancias))
+print('mediana distancia: ', np.median(distancias))
+
+y_parecidos = []
+
+for parecido in indices[game]:
+    print(parecido, ' : ', y[parecido])
+    if(parecido != game):
+        y_parecidos.append(y[parecido])
+    
+#print('vai pagar? ', np.bincount(y_parecidos).argmax())
+
+
